b45ee824fbe944100dd7d9201168109074220106
Roman Garanin <evilkhaoskat@gmail.com>
2018-03-25 21:20:06 +0400

    empty react app 'robofriends'

---
+++ robofriends/src/Hello.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './Hello.css';
+
+class Hello extends Component {
+  render() {
+    return (
+        <div className="f1 tc">
+          <h1>Hello !</h1>
+          <p>{this.props.greeting}</p>
+        </div>
+    )
+  }
+}
+
+export default Hello;
